2b8b21b21305d8c1608d53d6128d48ad24fcee4e
lee-orr <leeorr@gmail.com>
2021-12-14 21:43:27 +0000

    add draw plugin

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -1,3 +1,5 @@
+import MapboxDraw from '@mapbox/mapbox-gl-draw'
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
 import mapboxgl, { Map, NavigationControl } from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import type { ReactElement } from 'react'
@@ -8,6 +10,7 @@ mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN as string
 function MapElement(): ReactElement {
 	const mapReference = useRef<HTMLDivElement>(null)
 	const [map, setMap] = useState<Map>()
+	const [draw, setDraw] = useState<MapboxDraw>()
 	useEffect(() => {
 		if (!map && mapboxgl.accessToken && mapReference.current) {
 			const innerMap = new Map({
@@ -19,9 +22,12 @@ function MapElement(): ReactElement {
 				zoom: 2
 			})
 			innerMap.addControl(new NavigationControl())
+			const innerDraw = new MapboxDraw({})
+			innerMap.addControl(innerDraw)
 			setMap(innerMap)
+			setDraw(innerDraw)
 		}
-	}, [map])
+	}, [map, draw])
 	return <div className='w-full h-full' ref={mapReference} />
 }
 
