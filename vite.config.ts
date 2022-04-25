f4c4dba96c90644e4397e378db24902a503a1df4
lee-orr <leeorr@gmail.com>
2022-04-25 17:49:43 +0000

    adjust for better gitpod config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
 import eslintPlugin from '@nabla/vite-plugin-eslint'
 import react from '@vitejs/plugin-react'
 import istanbul from 'rollup-plugin-istanbul'
@@ -7,6 +9,18 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(({ mode }) => ({
 	base: './',
+	server: {
+		hmr: process.env.GITPOD_WORKSPACE_URL
+			? {
+					// Due to port fowarding, we have to replace
+					// 'https' with the forwarded port, as this
+					// is the URI created by Gitpod.
+					host: process.env.GITPOD_WORKSPACE_URL.replace('https://', '3000-'),
+					protocol: 'wss',
+					clientPort: 443
+			  }
+			: true
+	},
 	plugins: [
 		tsconfigPaths(),
 		react(),
